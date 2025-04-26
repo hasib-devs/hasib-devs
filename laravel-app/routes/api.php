fb36eed8b5a8e5f79927d83029b822de437f0f24
Hasibur Rahman <hasib.webdev@gmail.com>
2025-04-27 03:21:31 +0600

    Removed laravel and nuxt

--- laravel-app/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\NewsletterController;
-use Illuminate\Support\Facades\Route;
-
-Route::prefix("api/v1")->group(function () {
-    Route::post("/newsletter", [NewsletterController::class, "store"]);
-});
--- nuxt-app/nuxt.config.ts
+++
@@ -1,6 +0,0 @@
-export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  css: ['~/assets/main.scss'],
-  modules: ['@nuxtjs/tailwindcss'],
-});
