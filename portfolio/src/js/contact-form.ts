82bac09b4b620447bf599a183127ffd0d3521b88
Hasibur Rahman <hasib.webdev@gmail.com>
2025-04-27 07:54:12 +0600

    better ui

--- portfolio/src/js/contact-form.ts
+++ portfolio/src/js/contact-form.ts
@@ -2,17 +2,34 @@ import { $ } from "./helpers";
 
 const formEl = $<HTMLFormElement>("#contact_form");
 
+function hideLoadingOverlay(el: HTMLDivElement) {
+    el.classList.add('hidden');
+}
+function showLoadingOverlay(el: HTMLDivElement) {
+    el.classList.remove('hidden');
+    el.classList.add('flex');
+}
+
 formEl.addEventListener("submit", async function (e) {
     e.preventDefault();
 
     const form = (e.target as HTMLFormElement);
+    const loadingOverlay = form.querySelector<HTMLDivElement>('#loadingOverlay')!;
+    const successOverlay = form.querySelector<HTMLDivElement>('#successOverlay')!;
+    const failedOverlay = form.querySelector<HTMLDivElement>('#failedOverlay')!;
+
+    successOverlay.addEventListener("click", function (e) {
+        e.preventDefault();
+        successOverlay.classList.remove('flex');
+        successOverlay.classList.add('hidden');
+    });
 
     const formData = new FormData(form);
-    // const submitBtn = form.querySelector<HTMLButtonElement>('button[type="submit"]')!;
-    // submitBtn.disabled = true;
+
     const api_url = import.meta.env.PROD ? "https://api-worker.hasib-webdev.workers.dev" : "http://localhost:8787";
     const url = `${api_url}/api/contact`;
 
+    showLoadingOverlay(loadingOverlay);
     try {
         const response = await fetch(url, {
             method: "POST",
@@ -26,7 +43,10 @@ formEl.addEventListener("submit", async function (e) {
         console.log("Form submited successfully", await response.json());
         form.reset();
 
+        showLoadingOverlay(successOverlay);
     } catch (error) {
-        console.log("Failed to summit contact form.", error);
+        showLoadingOverlay(failedOverlay);
+    } finally {
+        hideLoadingOverlay(loadingOverlay);
     }
 });
