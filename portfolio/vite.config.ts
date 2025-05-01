9cfd8072d5100aafaf5d947fef64eda798f6b2f0
Hasibur Rahman <hasib.webdev@gmail.com>
2025-05-01 09:29:51 +0600

    wip

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+
 export default defineConfig({
     plugins: [
         tailwindcss(),
+        createHtmlPlugin({
+            minify: true,
+            entry: "src/js/main.ts"
+        })
     ],
 });
